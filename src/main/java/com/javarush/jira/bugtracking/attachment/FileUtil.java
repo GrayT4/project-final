2e2b0b84d9b92faca7da192566ee9e96df6e4916
Sergei T <tychinkinsergei@yandex.ru>
2023-12-26 23:26:44 +0500

    6 task complete: refactoring method

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -6,15 +6,9 @@ import lombok.experimental.UtilityClass;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 @UtilityClass
 public class FileUtil {
@@ -25,14 +19,17 @@ public class FileUtil {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
-            }
+        Path directory = Path.of(directoryPath);
+        try {
+            Files.createDirectories(directory);
+            Path filePath = directory.resolve(fileName);
+            Files.write(filePath, multipartFile.getBytes());
+        } catch (InvalidPathException e) {
+            throw new IllegalRequestDataException("Wrong path:" + directoryPath);
+        } catch (FileAlreadyExistsException e) {
+            throw new IllegalRequestDataException("File already exists: " + fileName);
+        } catch (IOException e) {
+            throw new IllegalRequestDataException("Failed to upload file: " + multipartFile.getOriginalFilename());
         }
     }
 
